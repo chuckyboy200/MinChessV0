4958b2bccdf9d7393e6b4d6e29bff087664057fb
chuckyboy200 <chuckyboy200@gmail.com>
2024-05-14 10:52:46 +1200

    New class to handle gui actions

---
+++ src/minchessv0/gui/ActionHandler.java
@@ -0,0 +1,17 @@
+package minchessv0.gui;
+
+public class ActionHandler {
+    
+    public ActionHandler() {
+
+    }
+
+    public void doWhitePlayer() {
+
+    }
+
+    public void doBlackPlayer() {
+        
+    }
+
+}
