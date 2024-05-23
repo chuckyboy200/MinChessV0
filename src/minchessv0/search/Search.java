34b580aeb368c018e5ccbd214a0b34485a3f81bd
chuckyboy200 <chuckyboy200@gmail.com>
2024-05-23 18:06:27 +1200

    Remove extraneous method

--- src/minchessv0/search/Search.java
+++ src/minchessv0/search/Search.java
@@ -2,7 +2,6 @@ package minchessv0.search;
 
 public interface Search {
     
-    public long beginSearch(long[] board, int maxDepth, int time);
     public void requestHalt();
     public long bestMove();
     public String pv();
