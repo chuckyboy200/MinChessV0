2e9ef738679f07ba257cb1c07e016bed0e1e5c11
chuckyboy200 <chuckyboy200@gmail.com>
2024-05-28 13:04:01 +1200

    Create new test class for search including Score and eval analysis

--- src/minchessv0/test/SearchParallelTest.java
+++ src/minchessv0/test/SearchParallelTest.java
@@ -1,13 +1,13 @@
 package minchessv0.test;
 
 import minchessv0.board.Board;
-import minchessv0.eval.Eval;
-import minchessv0.game.Game;
+import minchessv0.eval.Score;
 import minchessv0.gen.Gen;
 import minchessv0.move.Move;
 import minchessv0.search.Search;
 import minchessv0.sort.Sort;
 import minchessv0.util.Piece;
+import minchessv0.util.Value;
 
 public class SearchParallelTest implements Search, Runnable {
     
@@ -79,7 +79,7 @@ public class SearchParallelTest implements Search, Runnable {
     private int sendInfoDelay;
     private int currentSearchDepth;
     private long infoTimeElapsed;
-    private int currentBestScore;
+    private Score currentBestScore;
     private int bestScoreFoundAtDepth;
     private long currentDepthNodes;
     private long nextTimeToSendInfo;
@@ -89,16 +89,19 @@ public class SearchParallelTest implements Search, Runnable {
         this.rootMoveListLength = (int) this.rootMoveList[Gen.MOVELIST_SIZE];
         long[] boardAfterMove;
         long move;
+        Score score;
+        int eval;
+        int player = (int) board[Board.STATUS] & Board.PLAYER_BIT;
+        int other = 1 ^ player;
         for(int moveIndex = 0; moveIndex < this.rootMoveListLength; moveIndex ++) {
             move = this.rootMoveList[moveIndex];
             boardAfterMove = Board.makeMove(this.board, move);
-            int eval = -new Eval(boardAfterMove).eval();
+            score = new Eval(boardAfterMove).score();
+            eval = score.eval(player) - score.eval(other);
             this.rootMoveList[moveIndex] = (move & 0xffffffffL) | ((long) eval << 32);
-            System.out.println((int) (this.rootMoveList[moveIndex] >>> 32));
+            //System.out.println((int) (this.rootMoveList[moveIndex] >>> 32));
         }
-        System.out.println(Move.moveListString(this.rootMoveList));
         Sort.sort(this.rootMoveList);
-        System.out.println(Move.moveListString(this.rootMoveList));
     }
 
     private void doSearch() throws InterruptedException {
@@ -107,19 +110,21 @@ public class SearchParallelTest implements Search, Runnable {
         this.bestMove = 0L;
         this.startTime = System.currentTimeMillis();
         this.nextTimeToSendInfo = this.startTime + this.sendInfoDelay;
-        this.currentBestScore = -INFINITY;
+        this.currentBestScore = new Score(-INFINITY);
         this.bestScoreFoundAtDepth = 0;
         this.infoTimeElapsed = this.startTime;
         long[] boardAfterMove;
         long move;
+        Score score;
         int eval;
         int bestEval;
         int[] tempPV = new int[MAX_PV_LENGTH];
         int moveEval;
+        int player = (int) this.board[Board.STATUS] & Board.PLAYER_BIT;
         for(int depth = this.maxDepth < 2 ? this.maxDepth : 2; depth <= this.maxDepth; depth += (this.maxDepth - depth == 1) ? 1 : 2) {
             this.currentSearchDepth = depth;
             this.currentDepthNodes = this.nodesSearched;
-            if(this.currentBestScore > (-INFINITY / 2)) sendInfo();
+            if(this.currentBestScore.eval() > (-INFINITY / 2)) sendInfo();
             bestEval = -INFINITY;
             Sort.sort(this.rootMoveList);
             for(int moveIndex = 0; moveIndex < this.rootMoveListLength; moveIndex ++) {
@@ -127,16 +132,8 @@ public class SearchParallelTest implements Search, Runnable {
                 moveEval = (int) (move >> 32);
                 boardAfterMove = Board.makeMove(this.board, move);
                 tempPV[0] = 0;
-                eval = -search(boardAfterMove, depth, -INFINITY, INFINITY, tempPV);
-                int staticEval = new Eval(boardAfterMove).eval();
-                if(Math.abs(staticEval - eval) > 100) {
-                    System.out.println("Variant eval at depth " + depth + " move " + Move.string(move));
-                    System.out.println("board: static eval " + new Eval(board).eval());
-                    Board.drawText(board);
-                    System.out.println("boardAfterMove: static eval " + staticEval);
-                    Board.drawText(boardAfterMove);
-                    System.out.println("search eval " + eval);
-                }
+                score = search(boardAfterMove, depth, new Score(INFINITY).negate(), new Score(INFINITY), tempPV).negate();
+                eval = score.eval();
                 if(eval > moveEval) this.rootMoveList[moveIndex] = (move & 0xffffffffL) | ((long) eval << 32);
                 if(eval > bestEval) {
                     this.bestMove = move;
@@ -144,8 +141,9 @@ public class SearchParallelTest implements Search, Runnable {
                     this.rootPV[0] = tempPV[0] + 1;
                     this.rootPV[1] = (int) move;
                     System.arraycopy(tempPV, 1, this.rootPV, 2, tempPV[0]);
-                    this.currentBestScore = bestEval;
+                    this.currentBestScore = score;
                     this.bestScoreFoundAtDepth = depth;
+                    System.out.println(score.string());
                     sendInfo();
                 }
                 if(this.searchHalted || this.timeReached) break;
@@ -154,16 +152,20 @@ public class SearchParallelTest implements Search, Runnable {
         }
         sendInfo();
         this.searchRunning = false;
-        //Game.INSTANCE.sendCommand("searchcomplete");
     }
 
-    private int search(long[] board, int depth, int alpha, int beta, int[] pv) {
+    private Score search(long[] board, int depth, Score alpha, Score beta, int[] pv) {
+        //System.out.println("search depth " + depth + " alpha " + alpha.stringInfo() + " beta " + beta.stringInfo());
+        //try {Thread.sleep(1000);} catch(InterruptedException e) { e.printStackTrace(); }
         if(this.searchHalted || this.timeReached) return alpha;
         if(this.nextTimeToSendInfo < System.currentTimeMillis()) {
             this.nextTimeToSendInfo = System.currentTimeMillis() + this.sendInfoDelay;
             sendInfo();
         }
-        if(depth < 1) return quiesce(board, alpha, beta);
+        if(depth < 1) {
+            //System.out.println(" redirecting to quiesce\nalpha " + alpha.alphaBeta() + " beta " + beta.alphaBeta());
+            return quiesce(board, alpha, beta);
+        }
         this.nodesSearched ++;
         int player = (int) board[Board.STATUS] & Board.PLAYER_BIT;
         long[] localMoveList = Gen.gen(board, false, false);
@@ -173,34 +175,49 @@ public class SearchParallelTest implements Search, Runnable {
         int eval;
         int[] childPV = new int[MAX_PV_LENGTH];
         int bestEval = -INFINITY;
+        Score score;
         for(int moveIndex = 0; moveIndex < localMoveListLength; moveIndex ++) {
             move = localMoveList[moveIndex];
             boardAfterMove = Board.makeMove(board, move);
             if(Board.isPlayerInCheck(boardAfterMove, player)) continue;
             childPV[0] = 0;
-            eval = -search(boardAfterMove, depth - 1, -beta, -alpha,  childPV);
+            //System.out.println("move " + moveIndex + " " + Move.string(move));
+            score = new Score(search(boardAfterMove, depth - 1, new Score(beta.negate()), new Score(alpha.negate()),  childPV).negate());
+            //System.out.println("after search alpha " + alpha.stringInfo() + " beta " + beta.stringInfo() + " score " + score.stringInfo());
+            eval = score.eval();
             if(eval > bestEval) {
                 bestEval = eval;
                 pv[0] = childPV[0] + 1;
                 pv[1] = (int) move;
                 System.arraycopy(childPV, 1, pv, 2, childPV[0]);
-                if(eval >= beta) return beta;
-                if(eval > alpha) {
-                    alpha = eval;
+                if(eval >= beta.alphaBeta()) {
+                    //System.out.println("search returning beta\n" + beta.stringInfo());
+                    return beta;
+                }
+                if(eval > alpha.alphaBeta()) {
+                    alpha = new Score(score);
                 }
             }
         }
         if(System.currentTimeMillis() - this.startTime >= this.maxSearchTime) {
             this.timeReached = true;
         }
+        //System.out.println("search returning alpha\n" + alpha.stringInfo());
         return alpha;
     }
 
-    private int quiesce(long[] board, int alpha, int beta) {
-        int standPat = new Eval(board).eval();
-        if(standPat >= beta) return beta;
-        if(standPat + Piece.VALUE[Piece.QUEEN] < alpha) return alpha;
-        if(standPat > alpha) alpha = standPat;
+    private Score quiesce(long[] board, Score alpha, Score beta) {
+        Score standPat = new Eval(board).score();
+        //System.out.println("standpat\n" + standPat.stringInfo() + " beta " + beta.alphaBeta() + " alpha " + alpha.alphaBeta());
+        int standPatEval = standPat.eval();
+        if(standPatEval >= beta.alphaBeta()) return beta;
+        if(standPatEval + Piece.VALUE[Piece.QUEEN] < alpha.alphaBeta()) return alpha;
+        if(standPatEval > alpha.alphaBeta()) {
+            alpha = new Score(standPat);
+            //System.out.println("copying standPat to alpha");
+            //System.out.println("standPat =\n" + standPat.stringInfo());
+            //System.out.println("\nalpha =\n" + alpha.stringInfo());
+        }
         long[] localMoveList = Gen.gen(board, false, true);
         int localMoveListLength = (int) localMoveList[Gen.MOVELIST_SIZE];
         long move;
@@ -208,6 +225,7 @@ public class SearchParallelTest implements Search, Runnable {
         int player = (int) board[Board.STATUS] & Board.PLAYER_BIT;
         int other = 1 ^ player;
         int eval;
+        Score score;
         boolean otherMaterialPiecesMoreThanOne = Board.countMaterialPieces(board, other) > 1;
         for(int moveIndex = 0; moveIndex < localMoveListLength; moveIndex ++) {
             move = localMoveList[moveIndex];
@@ -216,21 +234,26 @@ public class SearchParallelTest implements Search, Runnable {
             }
             boardAfterMove = Board.makeMove(board, move);
             if(Board.isPlayerInCheck(boardAfterMove, player)) continue;
-            eval = -quiesce(boardAfterMove, -beta, -alpha);
-            if(eval >= beta) return beta;
-            if(eval > alpha) alpha = eval;
+            score = new Score(quiesce(boardAfterMove, new Score(beta.negate()), new Score(alpha.negate())).negate());
+            eval = score.eval();
+            if(eval >= beta.alphaBeta()) {
+                //System.out.println("quiesce returning beta\n" + beta.stringInfo());
+                return beta;
+            }
+            if(eval > alpha.alphaBeta()) alpha = new Score(score);
         }
+        //System.out.println("quiesce returning alpha\n" + alpha.stringInfo());
         return alpha;
     }
 
     private void sendInfo() {
         long currentTime = System.currentTimeMillis();
-        System.out.println("info depth " + this.currentSearchDepth);
-        System.out.println("info score cp " + this.currentBestScore + " depth " + this.bestScoreFoundAtDepth + " nodes " + (this.nodesSearched - this.currentDepthNodes) + " time " + (currentTime - this.infoTimeElapsed) + " pv " + pv());
+        //System.out.println("info depth " + this.currentSearchDepth);
+        //System.out.println("info score cp " + this.currentBestScore.eval() + " depth " + this.bestScoreFoundAtDepth + " nodes " + (this.nodesSearched - this.currentDepthNodes) + " time " + (currentTime - this.infoTimeElapsed) + " pv " + pv());
         this.infoTimeElapsed = currentTime;
         int elapsedSeconds = (int) ((currentTime - this.startTime) / 1000);
         long nps = elapsedSeconds > 0 ? this.nodesSearched / elapsedSeconds : this.nodesSearched;
-        System.out.println("info nps " + nps);
+        //System.out.println("info nps " + nps);
     }
 
 }
