43a3008dcb6eb7546fe8ba53d51311a8a850a970
chuckyboy200 <chuckyboy200@gmail.com>
2024-05-28 13:03:34 +1200

    Test Eval class

--- src/minchessv0/test/EvalTest.java
+++ src/minchessv0/test/EvalTest.java
@@ -1,14 +1,18 @@
 package minchessv0.test;
 
 import minchessv0.board.Board;
-import minchessv0.eval.EvalVerbose;
+import minchessv0.eval.Score;
+import minchessv0.move.Move;
 
 public class EvalTest {
     
     public static void test() {
         long[] board = Board.startingPosition();
+        long move = Move.stringToInt(board, Move.string(Move.convertStartTarget(12, 28)));
+        board = Board.makeMove(board, move);
         Board.drawText(board);
-        System.out.println(new EvalVerbose(board).eval());
+        SearchParallelTest search = new SearchParallelTest(board, 6, 10000);
+        search.run();
     }
 
     private EvalTest() {}
