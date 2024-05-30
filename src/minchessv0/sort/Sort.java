8eb5568d09fa57b1b2dfd21e9586d09bc65d5e3b
chuckyboy200 <chuckyboy200@gmail.com>
2024-05-30 17:50:11 +1200

    Add support for sorting list by capture value

--- src/minchessv0/sort/Sort.java
+++ src/minchessv0/sort/Sort.java
@@ -1,12 +1,54 @@
 package minchessv0.sort;
 
+import minchessv0.board.Board;
 import minchessv0.gen.Gen;
+import minchessv0.util.Piece;
 
 public class Sort {
     
     public static void sort(long[] array) {
         quickSort(array, 0, (int) array[Gen.MOVELIST_SIZE] - 1);
     }
+
+    public static void sortNoEval(long[] board, long[] array) {
+        if(array.length == 0) return;
+        long move;
+        long sortScore;
+        int startType;
+        int targetType;
+        int promoteType;
+        int targetSquare;
+        int other = (int) ((8 ^ array[0]) & 8) >>> 3;
+        int valueDifference;
+        int moveListSize = (int) array[Gen.MOVELIST_SIZE];
+        for(int i = 0; i < moveListSize; i ++) {
+            move = array[i];
+            sortScore = 0L;
+            startType = (int) move >>> Board.START_PIECE_SHIFT & Piece.TYPE;
+            targetType = (int) move >>> Board.TARGET_PIECE_SHIFT & Piece.TYPE;
+            promoteType = (int) move >>> Board.PROMOTE_PIECE_SHIFT & Piece.TYPE;
+            targetSquare = (int) move >>> Board.TARGET_SQUARE_SHIFT & Board.SQUARE_BITS;
+            if(promoteType != Piece.EMPTY) {
+                sortScore += PROMOTE_SCORE + Piece.VALUE[promoteType] + (targetType != Piece.EMPTY ? CAPTURE : 0);
+            } else {
+                if(targetType != Piece.EMPTY) {
+                    valueDifference = Piece.VALUE[targetType] - Piece.VALUE[startType];
+                    sortScore += valueDifference + (valueDifference > 50 ? HIGH + valueDifference : valueDifference > -50 ? LOW + valueDifference : !Board.isSquareAttackedByPlayer(board, targetSquare, other) ? LESS : 0);
+                }
+            }
+            array[i] = (move & 0xffffffffL) | (sortScore << 32);
+        }
+        quickSort(array, 0, moveListSize - 1);
+        for(int i = 0; i < moveListSize; i ++) {
+            array[i] &= 0xffffffffL;
+        }
+    }
+
+    private static final int PROMOTE_SCORE = 100000;
+    private static final int HIGH = 80000;
+    private static final int LOW = 60000;
+    private static final int CAPTURE = 20000;
+    private static final int LESS = 10000;
 
     private Sort() {}
 
